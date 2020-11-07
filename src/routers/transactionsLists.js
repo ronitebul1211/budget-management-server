27a015e47d0c02cf200864d54810b0f7fe28a3f4
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-07 23:17:13 +0200

    create transactions lists router

---
+++ src/routers/transactionsLists.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { models } = require("mongoose");
+const TransactionsList = require("../models/transactionsList");
+
+const router = new express.Router();
+
+module.exports = router;
