10e88c4e30865aa6688269401cffe6d9c4b0fdc2
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-07 17:59:29 +0200

    ADD: base files structure

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.listen(port, () => {
+   console.log(`Server up on port ${port}`);
+});
