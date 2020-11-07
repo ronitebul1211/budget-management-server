ff5c64b0bdb2b5c63d963c366bd2df8dc029ccb4
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-07 23:34:07 +0200

    implement GET to request transaction list for specific month, year

--- src/routers/transactionsLists.js
+++ src/routers/transactionsLists.js
@@ -1,7 +1,23 @@
 const express = require("express");
-const { models } = require("mongoose");
 const TransactionsList = require("../models/transactionsList");
 
 const router = new express.Router();
+
+/** GET transactions list by month, year
+ * (404) - List of transactions for the month and year does not exist
+ * (500) - other errors
+ */
+router.get("/api/transactions-lists/:year/:month", async (req, res) => {
+   try {
+      const { month, year } = req.params;
+      const transactionsList = await TransactionsList.findOne({ month, year });
+      if (!transactionsList) {
+         return res.status(404).send();
+      }
+      res.send(transactionsList);
+   } catch (err) {
+      res.status(500).send(err.message);
+   }
+});
 
 module.exports = router;
