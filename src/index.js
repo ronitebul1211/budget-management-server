97085a1b8ad54dca4c6b8618c41b99d391acca04
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-07 19:10:47 +0200

    connect mongo DB

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+require("./db/mongoose"); //RUN
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+
+app.get("/api/transactions-lists/:year/:month", (req, res) => {
+   const { year, month } = req.params;
+   res.send({ year, month });
+});
 
 app.listen(port, () => {
    console.log(`Server up on port ${port}`);
