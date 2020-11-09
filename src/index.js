b399e74fc396ca6d3e0cf1fae77affcea0e8b7a7
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-09 22:24:44 +0200

    add  cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 require("./db/mongoose"); //RUN
 const transactionsListsRouter = require("./routers/transactionsLists");
 
 const app = express();
 const port = process.env.PORT;
+app.use(cors());
 
 app.use(express.json());
 app.use(transactionsListsRouter);
