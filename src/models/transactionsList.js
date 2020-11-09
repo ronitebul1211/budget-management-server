d85d378be50db42c46e5d6776b5096d526541920
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-09 15:35:41 +0200

    add the functionality to get month status data of current transactions list

--- src/models/transactionsList.js
+++ src/models/transactionsList.js
@@ -52,6 +52,32 @@ const transactionsListSchema = mongoose.Schema({
    data: [transactionSchema],
 });
 
+/**
+ * @returns month status object contains data of credit, debit, and balance of current transactions list
+ */
+transactionsListSchema.methods.getMonthStatus = async function () {
+   const transactionTypeAgg = await TransactionsList.aggregate([
+      { $match: { _id: this._id } },
+      { $unwind: "$data" },
+      {
+         $group: {
+            _id: "$data.type",
+            sum: { $sum: "$data.totalPayment" },
+         },
+      },
+   ]);
+
+   const creditAgg = transactionTypeAgg.find((agg) => agg._id === "זכות");
+   const debitAgg = transactionTypeAgg.find((agg) => agg._id === "חובה");
+
+   const monthStatus = {
+      credit: !creditAgg ? 0 : creditAgg.sum,
+      debit: !debitAgg ? 0 : debitAgg.sum,
+   };
+   monthStatus.balance = monthStatus.credit - monthStatus.debit;
+   return monthStatus;
+};
+
 const TransactionsList = mongoose.model("Transactions-List", transactionsListSchema);
 
 module.exports = TransactionsList;
